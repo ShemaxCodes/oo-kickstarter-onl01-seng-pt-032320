4d1a6d39cc9459e96afc8839a76d84e40076da5e
Jashema Panapa <jcpanapa@gmail.com>
2020-04-22 03:40:01 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -9,7 +9,7 @@ class Backer
   
   def back_project(project)
     @backed_projects << project 
-     expect(hoverboard.backers).to include(logan)
+
      project.backers.include?(self)
     
   end 
